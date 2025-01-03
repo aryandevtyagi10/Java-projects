3c140e0ee53dc806598740e672e3fb7e5c96ef72
aryandevtyagi10 <168193058+aryandevtyagi10@users.noreply.github.com>
2025-01-03 20:47:52 +0530

    Add files via upload

---
+++ java basics/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args){
+//comments - myfirstjavaprogram!
+        /* sout+enter for shortcut
+        * multi line
+        * comment */
+        System.out.println("Hello World!");
+        System.out.println("\ni like pizza");
+        System.out.println("Finally bro");
+
+
+        int age = 21;
+        System.out.println(age);
+        double grade = 4.2;
+        System.out.println("Your Grade " +grade);
+        char alpha = 'A';
+        System.out.println(alpha);
+        boolean myDob = false;
+        System.out.println(myDob);
+
+        if(myDob){
+            System.out.println("You are eligible");
+        }
+        else
+            System.out.println("nah");
+        String name = "Aryan Dev Tyagi";
+        System.out.println("Hello "+name+ " " +age);
+        System.out.println(grade + " " + myDob );
+    }
+}
