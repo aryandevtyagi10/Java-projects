4ae25f55c74ac71cd8810863a35b647de26f0c13
ADT <168193058+aryandevtyagi10@users.noreply.github.com>
2025-06-25 22:51:16 +0530

    Create PrioritiyQueue.java

---
+++ Java-collection/PrioritiyQueue.java
@@ -0,0 +1,15 @@
+import java.util.PriorityQueue;
+
+public class PriorityQueueExample {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(40);
+        pq.add(10);
+        pq.add(30);
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.poll()); // Smallest element first
+        }
+    }
+}
