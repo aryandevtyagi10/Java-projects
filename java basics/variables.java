a87d68c98a8237e606af70fa3703f9035c444d06
ADT <168193058+aryandevtyagi10@users.noreply.github.com>
2025-07-01 22:20:30 +0530

    Create variables.java

---
+++ java basics/variables.java
@@ -0,0 +1,7 @@
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        double celsius = 36.6;
+        double fahrenheit = (celsius * 9/5) + 32;
+        System.out.println("Fahrenheit: " + fahrenheit);
+    }
+}
