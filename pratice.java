85a746e93685fee7da6bd1426e7d43d536c947c5
ADT <168193058+aryandevtyagi10@users.noreply.github.com>
2025-07-29 20:01:53 +0530

    Create pratice.java

---
+++ pratice.java
@@ -0,0 +1,19 @@
+// Getter for model
+public String getModel() {
+    return model;
+}
+
+// Setter for model
+public void setModel(String model) {
+    this.model = model;
+}
+
+// Getter for price
+public double getPrice() {
+    return price;
+}
+
+// Setter for price
+public void setPrice(double price) {
+    this.price = price;
+}
